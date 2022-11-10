40bac10ebbcfbefd75e330cffdc341bd47742444
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-11-09 22:58:54 -0700

    try new approach with bases (#2)
    * try new approach with bases
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/build_simple.sh
+++ .github/scripts/build_simple.sh
@@ -3,11 +3,9 @@
 set -e
 
 echo $PWD
-basedir=$(dirname $filename)
-printf "Base directory is ${basedir}\n"
 # Get relative path to PWD and generate dashed name from it
-echo "${prefix} -t ${container} ."
-${prefix} -f $filename -t ${container} .
-
+echo "${prefix} -t ${container} ${context}"
+${prefix} -t ${container} ${context}
 echo "uri=${container}" >> $GITHUB_OUTPUT
+basedir=$(dirname $filename)
 echo "dockerfile_dir=${basedir}" >> $GITHUB_OUTPUT
--- .github/scripts/relative_path.sh
+++ .github/scripts/relative_path.sh
@@ -3,6 +3,13 @@
 set -e
 
 echo "Original path is $filename"
+echo "Original container is $container"
 relative=$(echo ${filename/\/github\/workspace\//})
 echo "Relative path is $relative"
 echo "relative_path=${relative}" >> $GITHUB_OUTPUT
+container=$(echo ${container/-bases/""})
+echo "Container is $container"
+echo "container=${container}" >> $GITHUB_OUTPUT
+echo "container=${container}" >> $GITHUB_ENV
+
+
