3961125f3efa7d05d64c88c2398c5cd457b080a5
vsoch <vsoch@users.noreply.github.com>
2022-11-09 22:25:33 -0700

    try again?
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/relative_path.sh
+++ .github/scripts/relative_path.sh
@@ -3,6 +3,11 @@
 set -e
 
 echo "Original path is $filename"
+echo "Original container is $container"
 relative=$(echo ${filename/\/github\/workspace\//})
 echo "Relative path is $relative"
 echo "relative_path=${relative}" >> $GITHUB_OUTPUT
+container=$(echo "${container/bases-/""}")
+echo "container=${container}" >> $GITHUB_OUTPUT
+
+
